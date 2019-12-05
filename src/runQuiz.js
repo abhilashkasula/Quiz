fb2df1efad167697428f18c7e1fe1158356dd71e
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-12-05 12:05:25 +0530

    Added package.json and few more questions.

--- src/runQuiz.js
+++ src/runQuiz.js
@@ -4,39 +4,37 @@ const { printQuestion, isUserAnsCorrect } = require("../src/utils");
 stdin.setEncoding("utf8");
 stdin.setRawMode(true);
 
+const setIntervalToPrintTime = () => {
+  let timecount = 5;
+  return setInterval(() => {
+    stdout.cursorTo(15, 6);
+    stdout.clearLine();
+    stdout.write(`Seconds Remaining: ${--timecount}`);
+  }, 1000);
+};
+
 const runQuiz = () => {
   let count = 0;
   let correctAnsNo = 0;
   printQuestion(questionBank[count].question, questionBank[count].options);
   const askCurrentQuestion = usrAns => {
     clearTimeout(timer);
+    clearInterval(interval);
     const { answer } = questionBank[count];
     const isCorrect = isUserAnsCorrect(usrAns.trim(), answer);
     isCorrect && (correctAnsNo += 1);
     count += 1;
-    count == questionBank.length && process.exit(1);
+    count == questionBank.length && process.exit(0);
     const { question, options } = questionBank[count];
     printQuestion(question, options);
-    let timecount = 5;
-    let interval = setInterval(() => {
-      stdout.cursorTo(15, 6);
-      stdout.clearLine();
-      stdout.write(`Seconds Remaining: ${--timecount}`);
-    }, 1000);
+    interval = setIntervalToPrintTime();
     timer = setTimeout(() => {
-      clearInterval(interval);
       askCurrentQuestion("");
     }, 5000);
   };
   stdin.on("data", askCurrentQuestion);
-  let timecount = 5;
-  let interval = setInterval(() => {
-    stdout.cursorTo(15, 6);
-    stdout.clearLine();
-    stdout.write(`Seconds Remaining: ${--timecount}`);
-  }, 1000);
+  let interval = setIntervalToPrintTime();
   let timer = setTimeout(() => {
-    clearInterval(interval);
     askCurrentQuestion("");
   }, 5000);
   process.on("exit", () => {
